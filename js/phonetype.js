e15e4d06276bbd5c475a44d0ddd81f247f0456d2
liuying <liuyinghpu@163.com>
2018-09-02 20:48:31 +0800

    first commit

---
+++ js/phonetype.js
@@ -0,0 +1,14 @@
+var phone_type = '';
+if (/Android (\d+\.\d+)/.test(navigator.userAgent)) {
+	phone_type = "android";
+	var version = parseFloat(RegExp.$1);
+	if (version > 2.3) {
+		var phoneScale = parseInt(window.screen.width) / 750;
+		document.write('<meta name="viewport" content="width=750, minimum-scale = ' + phoneScale + ', maximum-scale = ' +
+			phoneScale + ', target-densitydpi=device-dpi">');
+	} else {
+		document.write('<meta name="viewport" content="width=750, target-densitydpi=device-dpi">');
+	}
+} else {
+	document.write('<meta name="viewport" content="width=750, user-scalable=no, target-densitydpi=device-dpi">');
+}
